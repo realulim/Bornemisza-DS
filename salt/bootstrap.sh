ba2d27a49b9e5c58af50de8f8e225f9ccf942dbb
Ulrich Mayring <ulim@mayring.de>
2017-04-06 23:02:51 +0200

    fix URIs

--- salt/bootstrap.sh
+++ salt/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SALT=/srv/salt
-BORNEY=https://github.com/realulim/Bornemisza
+BORNEY=https://github.com/realulim/Bornemisza/salt
 cd /opt
 
 # download and install salt
@@ -12,8 +12,8 @@ sed -i.bak 's/\#file_client: remote/file_client: local/g' /etc/salt/minion
 
 # create state tree
 mkdir -p $SALT/files/basics
-curl -o $SALT/top.sls -L $BORNEY
-curl -o $SALT/basics.sls -L $BORNEY
-curl -o $SALT/files/basics/bash.sh -L $BORNEY
+curl -o $SALT/top.sls -L $BORNEY/top.sls
+curl -o $SALT/basics.sls -L $BORNEY/basics.sls
+curl -o $SALT/files/basics/bash.sh -L $BORNEY/bash.sh
 
 salt-call state.highstate
