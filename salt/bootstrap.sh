3d8bc9469cc2b86be9c10f13dc241a61e6ff1829
Ulrich Mayring <ulim@mayring.de>
2017-04-06 23:35:37 +0200

    add yum update

--- salt/bootstrap.sh
+++ salt/bootstrap.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 SALT=/srv/salt
 BORNEY=https://github.com/realulim/Bornemisza/salt
-cd /opt
+
+# update system
+yum -y update
+yum clean all
 
 # download and install salt
+cd /opt
 curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
 sudo sh bootstrap-salt.sh git develop
 
@@ -16,4 +20,5 @@ curl -o $SALT/top.sls -L $BORNEY/top.sls
 curl -o $SALT/basics.sls -L $BORNEY/basics.sls
 curl -o $SALT/files/basics/bash.sh -L $BORNEY/bash.sh
 
+# create server
 salt-call state.highstate
