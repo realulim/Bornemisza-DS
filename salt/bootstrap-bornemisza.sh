880a099fddaf7ad7b095bd05cfbe5f02f866788f
Ulrich Mayring <ulim@mayring.de>
2017-04-13 16:07:49 +0200

    restructured directory tree

--- salt/bootstrap-bornemisza.sh
+++ salt/bootstrap-bornemisza.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-SALT=/srv/salt
-PILLAR=/srv/pillar
-BORNEY=https://raw.githubusercontent.com/realulim/Bornemisza/master/salt
+SaltLocal=/srv/salt
+PillarLocal=/srv/pillar
+SaltRemote=https://raw.githubusercontent.com/realulim/Bornemisza/master/salt/srv/salt
+PillarRemote=https://raw.githubusercontent.com/realulim/Bornemisza/master/salt/srv/pillar
 
 # create state tree
-mkdir -p $SALT/files/basics
-curl -o $SALT/top.sls -L $BORNEY/top.sls
-curl -o $SALT/basics.sls -L $BORNEY/basics.sls
-curl -o $SALT/files/basics/bash.sh -L $BORNEY/bash.sh
+mkdir -p $SaltLocal/files/basics
+cd $SaltLocal
+curl -L $SaltRemote/top.sls
+curl -L $SaltRemote/basics.sls
+curl -L $SaltRemote/files/basics/bash.sh
 
-mkdir -p $SALT/files/haproxy
-curl -o $SALT/haproxy.sls -L $BORNEY/haproxy/haproxy.sls
-curl -o $SALT/files/haproxy/haproxy.cfg -L $BORNEY/haproxy/haproxy.cfg
+mkdir -p $SaltLocal/files/haproxy
+curl -L $SaltRemote/haproxy.sls
+curl -L $SaltRemote/files/haproxy/haproxy.cfg
 
-mkdir -p $SALT/files/payara
-curl -o $SALT/payara.sls -L $BORNEY/payara/payara.sls
-curl -o $SALT/files/payara/payara.service -L $BORNEY/payara/payara.service
+mkdir -p $SaltLocal/files/payara
+curl -L $SaltRemote/payara.sls
+curl -L $SaltRemote/files/payara/payara.service
 
 # pillars
-mkdir -p $PILLAR
-curl -o $PILLAR/top.sls -L $BORNEY/pillar/top.sls
-curl -o $PILLAR/haproxy.sls -L $BORNEY/pillar/haproxy.sls
-sed -ie s/password:/"password: `openssl rand -base64 15`"/ $PILLAR/haproxy.sls
+mkdir -p $PillarLocal
+cd $PillarLocal
+curl -L $PillarRemote/top.sls
+curl -L $PillarRemote/haproxy.sls
+sed -ie s/password:/"password: `openssl rand -base64 15`"/ $PillarLocal/haproxy.sls
 
 # create server
 salt-call state.highstate
