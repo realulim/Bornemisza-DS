59f00f849d78efe1ea7d1eb82ce0dbc0fe6ff3af
Ulrich Mayring <ulim@mayring.de>
2017-04-25 23:59:36 +0200

    hosts file

--- salt/db/bootstrap-bornemisza.sh
+++ salt/db/bootstrap-bornemisza.sh
@@ -4,9 +4,15 @@ cd /opt
 source ./config.sh db
 
 # create state tree
-for FILE in top.sls hosts.sls
+for FILE in top.sls hosts.sls files/hosts
 do
 	curl -o $SaltLocal/$FILE -L $SaltRemote/$FILE
+done
+
+# static pillars
+for FILE in top.sls
+do
+        curl -o $PillarLocal/$FILE -L $PillarRemote/$FILE
 done
 
 # create server
