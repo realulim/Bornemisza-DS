c26c4b157f8e1550dfbe25b468e100f18d7fdeda
Ulrich Mayring <ulim@mayring.de>
2017-06-02 22:28:34 +0200

    function added

--- salt/db/bootstrap-bornemisza.sh
+++ salt/db/bootstrap-bornemisza.sh
@@ -15,6 +15,10 @@ do
 	curl -o $PillarLocal/$FILE -L $PillarRemote/$FILE
 done
 
+function getip {
+	host $1 | cut -d' ' -f4
+}
+
 # determine cluster members hostnames and ips
 if [ `grep hostname1 /srv/pillar/basics.sls | wc -l` -eq 0 ]; then
 	COUNTER=1
