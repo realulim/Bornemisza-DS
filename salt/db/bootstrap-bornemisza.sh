3184983c9af2bbbabda4a5965c34852d1fa59a25
Ulrich Mayring <ulim@mayring.de>
2017-06-27 00:36:20 +0200

    ssl fix

--- salt/db/bootstrap-bornemisza.sh
+++ salt/db/bootstrap-bornemisza.sh
@@ -29,10 +29,12 @@ if [ `grep hostname1 /srv/pillar/basics.sls | wc -l` -eq 0 ]; then
 fi
 
 # determine ssl hostname and domain
-SSLHOST=`domainname -f`
-printf "sslhost: $SSLHOST\n" | tee -a $PillarLocal/basics.sls
-SSLDOMAIN=`printf $SSLHOST | rev | awk -F. '{ print $1"."$2 }' | rev`
-printf "ssldomain: $SSLDOMAIN\n" | tee -a $PillarLocal/basics.sls
+if [ `grep ssl /srv/pillar/basics.sls | wc -l` -eq 0 ]; then
+	SSLHOST=`domainname -f`
+	printf "sslhost: $SSLHOST\n" | tee -a $PillarLocal/basics.sls
+	SSLDOMAIN=`printf $SSLHOST | rev | awk -F. '{ print $1"."$2 }' | rev`
+	printf "ssldomain: $SSLDOMAIN\n" | tee -a $PillarLocal/basics.sls
+fi
 
 # create server
 salt-call -l info state.highstate
