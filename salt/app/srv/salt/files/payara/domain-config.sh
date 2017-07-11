eb171178a2288c90c0639d6e95dbd2acf15fdfb8
Ulrich Mayring <ulim@mayring.de>
2017-07-11 23:21:49 +0200

    create custom jndi resource for couchdb _users database

--- salt/app/srv/salt/files/payara/domain-config.sh
+++ salt/app/srv/salt/files/payara/domain-config.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# $1 absolute path to asadmin binary
+# $2 absolute path to password file
+# $3 couchdb hostname available via https
+
 {% set ASADMIN_CMD='$1 --interactive=false --user admin --passwordfile=$2' %}
 
 # create alias for couchdb admin password
 {{ ASADMIN_CMD }} delete-password-alias couchdb-admin-password
 {{ ASADMIN_CMD }} create-password-alias couchdb-admin-password
+
+# create custom JNDI resource for CouchDB _users database
+{{ ASADMIN_CMD }} delete-custom-resource couchdb/Users
+{{ ASADMIN_CMD }} create-custom-resource --restype org.ektorp.CouchDbConnector --factoryclass de.bornemisza.users.da.CouchDbConnectorFactory --property url=https://$3 db=_users couchdb/Users
