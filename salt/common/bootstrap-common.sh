33407aecdae12543b0ebbd77e301dbec85cf517e
Ulrich Mayring <ulim@mayring.de>
2017-07-22 18:11:25 +0200

    fix

--- salt/common/bootstrap-common.sh
+++ salt/common/bootstrap-common.sh
@@ -43,14 +43,14 @@ for LOCATION in ${db_HostLocation[@]}
 do
 	DBHOSTNAME=$db_HostPrefix.$LOCATION.$db_Domain
 	SRVID=`curl -s "$CFAPI/$CFZONEID/dns_records" "$CF_AUTH_PARAMS" | jq '.result|.[]|select(.type=="SRV")|select(.data.target=="$DBHOSTNAME")|.id'| tr -d "\"`
-	SRVDATA="--data '{\"type\":\"SRV\",\"name\":\"_db._tcp.$SSLDOMAIN.\",\"content\":\"SRV 1 0 443 $DBHOSTNAME.\",\"data\":{\"priority\":1,\"weight\":0,\"port\":443,\"target\":\"$DBHOSTNAME\",\"service\":\"_db\",\"proto\":\"_tcp\",\"name\":\"$SSLDOMAIN\",\"ttl\":\"1\",\"proxied\":false}}'
+	SRVDATA=`echo '{"type":"SRV","name":"_db._tcp.$SSLDOMAIN.","content":"SRV 1 0 443 $DBHOSTNAME.","data":{"priority":1,"weight":0,"port":443,"target":"$DBHOSTNAME","service":"_db","proto":"_tcp","name":"$SSLDOMAIN","ttl":"1","proxied":false}}'`
 
 	if [ -n "$SRVID" ]; then
 		# record exist, so let's update it
-		curl -s -X PUT "$CFAPI/$CFZONEID/dns_records/$SRVID" "$CF_AUTH_PARAMS" "$SRVDATA"
+		curl -s -X PUT "$CFAPI/$CFZONEID/dns_records/$SRVID" "$CF_AUTH_PARAMS" --data '$SRVDATA'
 	else
 		# record does not exist, so let's create it
-		curl -s -X POST "$CFAPI/$CFZONEID/dns_records" "$CF_AUTH_PARAMS" "$SRVDATA"
+		curl -s -X POST "$CFAPI/$CFZONEID/dns_records" "$CF_AUTH_PARAMS" --data '$SRVDATA'
 	fi
 done
 
