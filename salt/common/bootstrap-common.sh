1407874732c85943e756f1486950e3264e853851
Ulrich Mayring <ulim@mayring.de>
2017-07-22 18:21:43 +0200

    fix

--- salt/common/bootstrap-common.sh
+++ salt/common/bootstrap-common.sh
@@ -36,23 +36,26 @@ CF_AUTH_PARAMS=`echo '-H "X-Auth-Email: $CFEMAIL" -H "X-Auth-Key: $CFKEY" -H "Co
 if [ `grep CFZONEID: /srv/pillar/basics.sls | wc -l` -eq 0 ]; then
 	CFZONEID=`curl -s "$CFAPI" "$CF_AUTH_PARAMS" | jq '.result|.[]|.id' | tr -d "\""`
 	printf "CFZONEID: $CFZONEID\n" | tee -a $PillarLocal/basics.sls
+
+	# write SRV records for domain
+	for LOCATION in ${db_HostLocation[@]}
+	do
+		DBHOSTNAME=$db_HostPrefix.$LOCATION.$db_Domain
+echo DEBUG
+echo "$CFAPI/$CFZONEID/dns_records"
+echo DEBUG
+		SRVID=`curl -s "$CFAPI/$CFZONEID/dns_records" "$CF_AUTH_PARAMS" | jq '.result|.[]|select(.type=="SRV")|select(.data.target=="$DBHOSTNAME")|.id'| tr -d "\"`
+		SRVDATA=`echo '{"type":"SRV","name":"_db._tcp.$SSLDOMAIN.","content":"SRV 1 0 443 $DBHOSTNAME.","data":{"priority":1,"weight":0,"port":443,"target":"$DBHOSTNAME","service":"_db","proto":"_tcp","name":"$SSLDOMAIN","ttl":"1","proxied":false}}'`
+
+		if [ -n "$SRVID" ]; then
+			# record exist, so let's update it
+			curl -s -X PUT "$CFAPI/$CFZONEID/dns_records/$SRVID" "$CF_AUTH_PARAMS" --data '$SRVDATA'
+		else
+			# record does not exist, so let's create it
+			curl -s -X POST "$CFAPI/$CFZONEID/dns_records" "$CF_AUTH_PARAMS" --data '$SRVDATA'
+		fi
+	done
 fi
-
-# write SRV records for domain
-for LOCATION in ${db_HostLocation[@]}
-do
-	DBHOSTNAME=$db_HostPrefix.$LOCATION.$db_Domain
-	SRVID=`curl -s "$CFAPI/$CFZONEID/dns_records" "$CF_AUTH_PARAMS" | jq '.result|.[]|select(.type=="SRV")|select(.data.target=="$DBHOSTNAME")|.id'| tr -d "\"`
-	SRVDATA=`echo '{"type":"SRV","name":"_db._tcp.$SSLDOMAIN.","content":"SRV 1 0 443 $DBHOSTNAME.","data":{"priority":1,"weight":0,"port":443,"target":"$DBHOSTNAME","service":"_db","proto":"_tcp","name":"$SSLDOMAIN","ttl":"1","proxied":false}}'`
-
-	if [ -n "$SRVID" ]; then
-		# record exist, so let's update it
-		curl -s -X PUT "$CFAPI/$CFZONEID/dns_records/$SRVID" "$CF_AUTH_PARAMS" --data '$SRVDATA'
-	else
-		# record does not exist, so let's create it
-		curl -s -X POST "$CFAPI/$CFZONEID/dns_records" "$CF_AUTH_PARAMS" --data '$SRVDATA'
-	fi
-done
 
 if [[ -e /opt/bootstrap.sh ]]; then
 	mv /opt/bootstrap.sh /opt/scripts
