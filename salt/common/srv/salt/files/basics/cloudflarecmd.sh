b88b11896ee43afd895ba3b9f00a86baad216b69
Ulrich Mayring <ulim@mayring.de>
2017-07-23 17:40:29 +0200

    fix

--- salt/common/srv/salt/files/basics/cloudflarecmd.sh
+++ salt/common/srv/salt/files/basics/cloudflarecmd.sh
@@ -7,7 +7,7 @@
 # $5 data to send (only with POST or PUT)
 
 if [ -n "$5" ]; then
-	/usr/bin/curl -s -X $1 "$2" -H "X-Auth-Email: $3" -H "X-Auth-Key: $4" -H "Content-Type: application/json" --data "$5"
+	/usr/bin/curl -s -X $1 "$2" -H "X-Auth-Email: $3" -H "X-Auth-Key: $4" -H "Content-Type: application/json" --data '$5'
 else
 	/usr/bin/curl -s -X $1 "$2" -H "X-Auth-Email: $3" -H "X-Auth-Key: $4" -H "Content-Type: application/json"
 fi
