b5fd3b7f9d83927b516dfb70d7b2a450cbd9b5c1
Ulrich Mayring <ulim@mayring.de>
2017-07-23 17:47:03 +0200

    fix

--- salt/db/bootstrap-bornemisza.sh
+++ salt/db/bootstrap-bornemisza.sh
@@ -7,7 +7,7 @@ sh bootstrap-common.sh db
 # create state tree
 mkdir -p $SaltLocal/files/couchdb
 mkdir -p $SaltLocal/files/haproxy
-for FILE in	top.sls dns.sls files/basics/cloudflarecmd.sh files/haproxy/haproxy.cfg hosts.sls ufw.sls files/hosts \
+for FILE in	top.sls dns.sls files/haproxy/haproxy.cfg hosts.sls ufw.sls files/hosts \
 		couchdb.sls files/couchdb/couchdb.service files/couchdb/netrc files/couchdb/vm.args files/couchdb/couchdb.logrotate.conf
 do
 	curl -o $SaltLocal/$FILE -L $SaltRemote/$FILE
