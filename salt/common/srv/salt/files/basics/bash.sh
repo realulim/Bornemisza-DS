8c6ed1c754fbf5104b42bb6f7bebd2c70422fa14
Ulrich Mayring <ulim@mayring.de>
2017-07-27 23:01:43 +0200

    sc alias

--- salt/common/srv/salt/files/basics/bash.sh
+++ salt/common/srv/salt/files/basics/bash.sh
@@ -10,6 +10,7 @@ alias p-='systemctl stop payara'
 alias p+='systemctl start payara'
 alias rm='rm -i'
 alias sbr='source /etc/profile.d/bash.sh'
+alias sc='salt-call'
 alias x='exit'
 
 PS1="\u@\H:\w => ";
