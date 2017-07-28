d7c7aee9f1cf62aa0abbf0a0c871ce28542548ce
Ulrich Mayring <ulim@mayring.de>
2017-07-28 19:19:20 +0200

    revert

--- salt/app/bootstrap-bornemisza.sh
+++ salt/app/bootstrap-bornemisza.sh
@@ -5,7 +5,7 @@ source ./config.sh app
 sh bootstrap-common.sh app
 
 # create app state tree
-svn export --force $SaltTrunk/app/srv /srv
+svn export --force $SaltTrunk/app/srv/salt /srv/salt
 
 # static pillars
 #for FILE in top.sls
