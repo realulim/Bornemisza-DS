e0a6c3e21bfdc5e08c2950f5ecefa3262bcc7d5a
Ulrich Mayring <ulim@mayring.de>
2017-08-30 21:13:44 +0200

    clean code

--- java/Status/src/main/java/de/bornemisza/status/JAXRSConfiguration.java
+++ java/Status/src/main/java/de/bornemisza/status/JAXRSConfiguration.java
@@ -4,10 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
- * JAX-RS resources in your application.
- *
- * @author airhacks.com
+ * Configures a JAX-RS endpoint.
  */
 @ApplicationPath("/")
 public class JAXRSConfiguration extends Application {
