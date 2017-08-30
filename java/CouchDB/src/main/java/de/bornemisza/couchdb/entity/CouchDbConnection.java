cef1285e11b2c821e6ad9df360058ccc22174dce
Ulrich Mayring <ulim@mayring.de>
2017-08-30 18:18:50 +0200

    fix

--- java/CouchDB/src/main/java/de/bornemisza/couchdb/entity/CouchDbConnection.java
+++ java/CouchDB/src/main/java/de/bornemisza/couchdb/entity/CouchDbConnection.java
@@ -1,14 +1,13 @@
 package de.bornemisza.couchdb.entity;
 
 import java.net.URL;
-import java.util.Objects;
 
 public class CouchDbConnection {
 
-    private URL url;
-    private String databaseName;
-    private String userName;
-    private String password;
+    private final URL url;
+    private final String databaseName;
+    private final String userName;
+    private final String password;
 
     public CouchDbConnection(URL url, String databaseName, String userName, String password) {
         this.url = url;
@@ -21,66 +20,16 @@ public class CouchDbConnection {
         return url;
     }
 
-    public void setUrl(URL url) {
-        this.url = url;
-    }
-
     public String getDatabaseName() {
         return databaseName;
-    }
-
-    public void setDatabaseName(String databaseName) {
-        this.databaseName = databaseName;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 47 * hash + Objects.hashCode(this.url);
-        hash = 47 * hash + Objects.hashCode(this.databaseName);
-        hash = 47 * hash + Objects.hashCode(this.userName);
-        hash = 47 * hash + Objects.hashCode(this.password);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final CouchDbConnection other = (CouchDbConnection) obj;
-        if (!Objects.equals(this.databaseName, other.databaseName)) {
-            return false;
-        }
-        if (!Objects.equals(this.userName, other.userName)) {
-            return false;
-        }
-        if (!Objects.equals(this.password, other.password)) {
-            return false;
-        }
-        return Objects.equals(this.url, other.url);
     }
 
     @Override
