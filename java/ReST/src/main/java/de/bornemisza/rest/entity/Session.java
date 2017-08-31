c94520af7541f1324d4bae7604045ab89aad855b
Ulrich Mayring <ulim@mayring.de>
2017-09-01 00:04:02 +0200

    completed fields

--- java/ReST/src/main/java/de/bornemisza/rest/entity/Session.java
+++ java/ReST/src/main/java/de/bornemisza/rest/entity/Session.java
@@ -1,6 +1,9 @@
 package de.bornemisza.rest.entity;
 
-import org.ektorp.support.CouchDbDocument;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -8,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Session extends CouchDbDocument {
+public class Session {
 
     public Session() {
         // JAXB needs this
@@ -18,12 +21,45 @@ public class Session extends CouchDbDocument {
     @JsonProperty(value = "type")
     private String type = "session";
 
+    @JsonProperty(value = "cookie")
+    private String cookie;
+
+    @JsonProperty(value = "name")
+    private String principal;
+
+    @JsonProperty(value = "roles")
+    private final List<String> roles = new ArrayList<>();
+
     public String getType() {
         return type;
     }
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(String cookie) {
+        this.cookie = cookie;
+    }
+
+    public String getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(@NotNull String name) {
+        this.principal = name;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void addRole(@NotNull String role) {
+        this.roles.add(role);
     }
 
 }
