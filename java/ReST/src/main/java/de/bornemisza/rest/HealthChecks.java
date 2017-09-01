800a12c88b80e180ebc1751f7b963dbe9453d5f2
Ulrich Mayring <ulim@mayring.de>
2017-09-02 00:08:17 +0200

    implementation

--- java/ReST/src/main/java/de/bornemisza/rest/HealthChecks.java
+++ java/ReST/src/main/java/de/bornemisza/rest/HealthChecks.java
@@ -1,9 +1,16 @@
 package de.bornemisza.rest;
 
+import java.util.List;
+import java.util.Map;
+import org.javalite.http.Post;
+
 public class HealthChecks {
 
     public boolean isCouchDbReady(Http http) {
-        return true;
+        Post post = http.post("");
+        Map<String, List<String>> headers = post.headers();
+        List<String> header = headers.get("Server");
+        return header != null && !header.isEmpty() && header.get(0).startsWith("CouchDB");
     }
 
 }
