05f28ac5bc4f0a14d412f4a639ecd230407cb88c
Ulrich Mayring <ulim@mayring.de>
2017-09-16 01:05:15 +0200

    first working ajax requests

--- html5/js/main.js
+++ html5/js/main.js
@@ -1,3 +1,8 @@
 function flipDialog() {
     document.getElementById('card').classList.toggle("flipped");
 }
+function createAuthorizationHeader(user, password) {
+    var tok = user + ':' + password;
+    var hash = btoa(tok);
+    return "Basic " + hash;
+}
