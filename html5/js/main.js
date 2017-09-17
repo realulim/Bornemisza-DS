b01a44ce952d3d9952aa53616a899970d8806b27
Ulrich Mayring <ulim@mayring.de>
2017-09-17 23:21:16 +0200

    create authentication failed animation

--- html5/js/main.js
+++ html5/js/main.js
@@ -7,6 +7,17 @@ var ERROR = "Error"
 function flipDialog() {
     document.getElementById('card').classList.toggle("flipped");
 }
+function displayMessage(show, elemId) {
+    var elem = document.getElementById(elemId)
+    if (show) elem.classList.remove("hidden")
+    else elem.classList.add("hidden")
+}
+function shakeElement(elemId) {
+    document.getElementById(elemId).classList.add("shake");
+    window.setTimeout(function () {
+        document.getElementById(elemId).classList.remove("shake");
+    }, 1000);
+}
 function createAuthorizationHeader(user, password) {
     var tok = user + ':' + password;
     var hash = btoa(tok);
