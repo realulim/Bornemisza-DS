d0a83777d8da99dd70a38df72824ddc7152e9168
Ulrich Mayring <ulim@mayring.de>
2017-09-24 17:11:09 +0200

    refactoring spinner into custom tag

--- html5/js/globals.js
+++ html5/js/globals.js
@@ -1,12 +1,14 @@
 // Custom Events
-var AUTH_SUCCESS = "Authentication successful"
-var AUTH_FAILED = "Authentication failed"
-var AUTH_ERROR = "Error while authenticating"
+var AUTH_SUCCESS = "Authentication successful";
+var AUTH_FAILED = "Authentication failed";
+var AUTH_ERROR = "Error while authenticating";
+var LOADING_IN_PROGRESS = "Loading Data in Progress";
+var LOADING_DONE = "Loading Data completed";
 
 // Functions
 function shakeElement(elem) {
-    elem.classList.add("shake")
+    elem.classList.add("shake");
     window.setTimeout(function () {
-        elem.classList.remove("shake")
+        elem.classList.remove("shake");
     }, 1000);
 }
