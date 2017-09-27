30656fbde66dd0a9a4d80781011cda281ec4c67b
Ulrich Mayring <ulim@mayring.de>
2017-09-27 13:43:00 +0200

    no relation to project

--- bash/installUnixBench.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Helper script to install UnixBench on CentOS (for benchmarking a VPS before deciding to use this hoster)
-# Start benchmark via ./Run from inside UnixBench subdirectory
-
-yum install -y gcc gcc-c++ make libXext-devel && yum groupinstall -y "Development Tools" && yum install -y libX11-devel mesa-libGL-devel perl-Time-HiRes && cd /opt && git clone https://github.com/kdlucas/byte-unixbench.git && cd byte-unixbench/UnixBench && make
--- bash/installWrk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Helper script to install wrk on CentOS (for generating load)
-
-yum groupinstall -y "Development Tools" && yum install -y openssl-devel git && cd /opt && git clone https://github.com/wg/wrk.git && cd wrk && make
