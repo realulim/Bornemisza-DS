c7a99357e41d1e7d673a638ad7f206e26507ab90
Ulrich Mayring <ulim@mayring.de>
2017-09-28 01:38:19 +0200

    add urls

--- salt/app/srv/salt/files/frontend/config.js
+++ salt/app/srv/salt/files/frontend/config.js
@@ -1,4 +1,6 @@
 var config = {
 	urlNewSession: 'https://{{ pillar['sslhost'] }}/sessions/new',
 	urlActiveSession: 'https://{{ pillar['sslhost'] }}/sessions/active'
+	urlEndSession: 'https://{{ pillar['sslhost'] }}/sessions',
+	urlUuid: 'https://{{ pillar['sslhost'] }}/sessions/uuid'
 }
