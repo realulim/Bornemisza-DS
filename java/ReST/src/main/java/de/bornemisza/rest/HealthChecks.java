2c37c4b4efd70410d443e7af0e0ab33e9c2a8245
Ulrich Mayring <ulim@mayring.de>
2017-09-29 23:38:32 +0200

    do not throw exception when health check fails!

--- java/ReST/src/main/java/de/bornemisza/rest/HealthChecks.java
+++ java/ReST/src/main/java/de/bornemisza/rest/HealthChecks.java
@@ -2,15 +2,23 @@ package de.bornemisza.rest;
 
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
+
 import org.javalite.http.Post;
 
 public class HealthChecks {
 
     public boolean isCouchDbReady(Http http) {
-        Post post = http.post("");
-        Map<String, List<String>> headers = post.headers();
-        List<String> header = headers.get("Server");
-        return header != null && !header.isEmpty() && header.get(0).startsWith("CouchDB");
+        try {
+            Post post = http.post("");
+            Map<String, List<String>> headers = post.headers();
+            List<String> header = headers.get("Server");
+            return header != null && !header.isEmpty() && header.get(0).startsWith("CouchDB");
+        }
+        catch (Exception e) {
+            Logger.getAnonymousLogger().warning("CouchDB not ready: " + e.toString());
+            return false;
+        }
     }
 
 }
