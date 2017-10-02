5bbcb95c085e27a297015dc6017666b3d9507978
Ulrich Mayring <ulim@mayring.de>
2017-10-02 19:30:45 +0200

    implement UI and backend skeleton for continuous mode

--- html5/js/globals.js
+++ html5/js/globals.js
@@ -4,10 +4,12 @@ var AUTH_FAILED = "Authentication failed";
 var AUTH_ERROR = "Error while authenticating";
 var LOADING_IN_PROGRESS = "Loading Data in Progress";
 var LOADING_DONE = "Loading Data completed";
-var LOADING_ERROR = "Error while loading Data"
-var JUST_MARRIED = "User just logged in successfully"
-var ENABLE_SPINNER = "Enable Loading Indicator"
-var DISABLE_SPINNER = "Disable Loading Indicator"
+var LOADING_ERROR = "Error while loading Data";
+var JUST_MARRIED = "User just logged in successfully";
+var ENABLE_SPINNER = "Enable Loading Indicator";
+var DISABLE_SPINNER = "Disable Loading Indicator";
+var START_CONTINUOUS = "Start Continuous Mode";
+var STOP_CONTINUOUS = "Stop Continuous Mode";
 
 // Functions
 function setBackground(color) {
