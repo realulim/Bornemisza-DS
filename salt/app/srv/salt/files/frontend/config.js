d8b0d48b897a805e2c508891c316cdc49b11dcea
Ulrich Mayring <ulim@mayring.de>
2017-10-19 09:09:28 +0200

    fix

--- salt/app/srv/salt/files/frontend/config.js
+++ salt/app/srv/salt/files/frontend/config.js
@@ -1,7 +1,7 @@
 var config = {
 	urlNewSession: 'https://{{ pillar['sslhost'] }}/sessions/new',
 	urlActiveSession: 'https://{{ pillar['sslhost'] }}/sessions/active',
-	urlEndSession: 'https://{{ pillar['sslhost'] }}/sessions',
+	urlEndSession: 'https://{{ pillar['sslhost'] }}/sessions/',
 	urlUuid: 'https://{{ pillar['sslhost'] }}/sessions/uuid',
-	urlUsers: 'https://{{ pillar['sslhost'] }}/users'
+	urlUsers: 'https://{{ pillar['sslhost'] }}/users/'
 }
