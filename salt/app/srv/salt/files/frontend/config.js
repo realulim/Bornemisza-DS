774d9b488a36103015c123906b0d8feb2ee8758c
Ulrich Mayring <ulim@mayring.de>
2017-10-19 09:05:04 +0200

    new url

--- salt/app/srv/salt/files/frontend/config.js
+++ salt/app/srv/salt/files/frontend/config.js
@@ -2,5 +2,6 @@ var config = {
 	urlNewSession: 'https://{{ pillar['sslhost'] }}/sessions/new',
 	urlActiveSession: 'https://{{ pillar['sslhost'] }}/sessions/active',
 	urlEndSession: 'https://{{ pillar['sslhost'] }}/sessions',
-	urlUuid: 'https://{{ pillar['sslhost'] }}/sessions/uuid'
+	urlUuid: 'https://{{ pillar['sslhost'] }}/sessions/uuid',
+	urlUsers: 'https://{{ pillar['sslhost'] }}/sessions/users'
 }
