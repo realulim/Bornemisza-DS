1f303376f6f0a4b2b644d580871b6e985bba837f
Ulrich Mayring <ulim@mayring.de>
2017-10-28 23:39:07 +0200

    fix

--- salt/common/srv/salt/files/basics/checkForDeletedSrvRecords.sh
+++ salt/common/srv/salt/files/basics/checkForDeletedSrvRecords.sh
@@ -6,11 +6,11 @@ service={{ pillar['service'] }}
 
 records=`host -t srv _$service._tcp.$domain.|cut -d" " -f8`
 
-lines=`grep hostname /opt/test.sls|cut -d" " -f2`
+lines=`grep hostname /srv/pillar/haproxy.sls|cut -d" " -f2`
 
 for line in $lines ; do
 	if [[ ! "$records" =~ "$line" ]] ; then
 		printf "[`date`] Gone: $line\n" >> /opt/logs/srvRecords.log
-		sed -i "/$line/d" /opt/test.sls
+		sed -i "/$line/d" /srv/pillar/haproxy.sls
 	fi
 done
