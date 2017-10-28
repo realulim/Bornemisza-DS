77415d9a11a0e8eadb809fcdce6f30b99945adb7
Ulrich Mayring <ulim@mayring.de>
2017-10-28 18:02:04 +0200

    fix

--- salt/db/bootstrap-bornemisza.sh
+++ salt/db/bootstrap-bornemisza.sh
@@ -39,9 +39,6 @@ if [ ! -e $PillarLocal/couchdb.sls ]; then
 	printf "\n"
 fi
 
-# cluster size needs to be rewritten on every run
-printf "clustersize: `host -t srv _db._tcp.$domain.|wc -l`\n" | tee $PillarLocal/cluster.sls
-
 # letsencrypt needs to know the ssl endpoint for creating its certificate
 if ! grep -q sslhost: /srv/pillar/basics.sls ; then
 	SSLHOST=`domainname -f`
