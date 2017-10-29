011ea9b275af34138b4e5135e41824370a5c4c6f
Ulrich Mayring <ulim@mayring.de>
2017-10-29 19:16:17 +0100

    run highstate after srv record was deleted from pillar

--- salt/common/srv/salt/files/basics/checkForDeletedSrvRecords.sh
+++ salt/common/srv/salt/files/basics/checkForDeletedSrvRecords.sh
@@ -8,9 +8,15 @@ records=`host -t srv _$service._tcp.$domain.|cut -d" " -f8`
 
 lines=`grep hostname /srv/pillar/haproxy.sls|cut -d" " -f2`
 
+highstate=false
 for line in $lines ; do
 	if [[ ! "$records" =~ "$line" ]] ; then
 		printf "[`date`] Gone: $line\n" >> /opt/logs/srvRecords.log
 		sed -i "/$line/d" /srv/pillar/haproxy.sls
+		highstate=true
 	fi
 done
+
+if [ "$highstate" = true ]; then
+	/usr/bin/bash -c '/usr/bin/salt-call -l error state.highstate >> /opt/logs/highstate.log'
+fi
--- salt/db/srv/salt/files/checkForDeletedAppSrvRecords.sh
+++ salt/db/srv/salt/files/checkForDeletedAppSrvRecords.sh
@@ -7,9 +7,15 @@ records=`host -t srv _app._tcp.$domain.|cut -d" " -f8|xargs -L1 host|cut -d" " -
 
 lines=`grep ipapp /srv/pillar/haproxy.sls|cut -d" " -f2`
 
+highstate=false
 for line in $lines ; do
 	if [[ ! "$records" =~ "$line" ]] ; then
 		printf "[`date`] Gone: $line\n" >> /opt/logs/srvRecords.log
 		sed -i "/$line/d" /srv/pillar/haproxy.sls
+		highstate=true
 	fi
 done
+
+if [ "$highstate" = true ]; then
+	/usr/bin/bash -c '/usr/bin/salt-call -l error state.highstate >> /opt/logs/highstate.log'
+fi
