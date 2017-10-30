67869f5a2d9807abcb7e084ac9ffce4e8218fdad
Ulrich Mayring <ulim@mayring.de>
2017-10-30 23:11:21 +0100

    fix

--- salt/db/srv/salt/files/checkForDeletedAppSrvRecords.sh
+++ salt/db/srv/salt/files/checkForDeletedAppSrvRecords.sh
@@ -5,13 +5,13 @@ domain={{ pillar['ssldomain'] }}
 
 records=`host -t srv _app._tcp.$domain.|cut -d" " -f8|xargs -L1 host|cut -d" " -f4`
 
-lines=`grep ipapp /srv/pillar/haproxy.sls|cut -d" " -f2`
+lines=`grep '-' /srv/pillar/appserverips.sls|cut -d" " -f4`
 
 highstate=false
 for line in $lines ; do
 	if [[ ! "$records" =~ "$line" ]] ; then
 		printf "[`date`] Gone: $line\n" >> /opt/logs/srvRecords.log
-		sed -i "/$line/d" /srv/pillar/haproxy.sls
+		sed -i "/$line/d" /srv/pillar/appserverips.sls
 		highstate=true
 	fi
 done
