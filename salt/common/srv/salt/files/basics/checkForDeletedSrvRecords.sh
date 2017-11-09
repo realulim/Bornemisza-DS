034f052df9d036b172956dfa64ae94cd77670ef4
Ulrich Mayring <ulim@mayring.de>
2017-11-09 20:11:15 +0100

    shellcheck fixes

--- salt/common/srv/salt/files/basics/checkForDeletedSrvRecords.sh
+++ salt/common/srv/salt/files/basics/checkForDeletedSrvRecords.sh
@@ -4,14 +4,14 @@ IFS=$'\n'
 domain={{ pillar['ssldomain'] }}
 service={{ pillar['service'] }}
 
-records=`host -t srv _${service}._tcp.$domain.|cut -d" " -f8`
-lines=`grep '-' /srv/pillar/${service}servers.sls|cut -d" " -f4`
+records=$(host -t srv _${service}._tcp.$domain.|cut -d" " -f8)
+lines=$(grep '-' /srv/pillar/"${service}"servers.sls|cut -d" " -f4)
 
 highstate=false
 for line in $lines ; do
-	if [[ ! "$records" =~ "$line" ]] ; then
-		printf "[`date`] Gone: $line\n" >> /opt/logs/srvRecords.log
-		sed -i "/$line/d" /srv/pillar/${service}servers.sls
+	if [[ ! "$records" == *"$line"* ]] ; then
+		printf "[$(date)] Gone: %s\n" "$line" >> /opt/logs/srvRecords.log
+		sed -i "/$line/d" /srv/pillar/"${service}"servers.sls
 		highstate=true
 	fi
 done
