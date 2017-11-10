b322a0fca869a544955004c4b477a5c0f6f57dcd
Ulrich Mayring <ulim@mayring.de>
2017-11-11 00:46:16 +0100

    fix

--- salt/db/srv/salt/files/couchdb/ddoc.sh
+++ salt/db/srv/salt/files/couchdb/ddoc.sh
@@ -8,6 +8,6 @@ REV=$(/usr/bin/curl -s -u "$AUTH" "$1"/_users/_design/User|jq -re '._rev')
 if [ "$REV" = 'null' ]; then
 	/usr/bin/curl -s -u "$AUTH" -X PUT "$1"/_users/_design/User -d '@/home/couchpotato/ddoc/User.json'
 else
-	DDOC=$(> /home/couchpotato/ddoc/User.json sed "s/_design\/User\",/_design\/User\",\"_rev\": \"$REV\",/g")
+	DDOC=$(< /home/couchpotato/ddoc/User.json sed "s/_design\/User\",/_design\/User\",\"_rev\": \"$REV\",/g")
 	/usr/bin/curl -s -u "$AUTH" -X PUT "$1"/_users/_design/User -d "$DDOC"
 fi
