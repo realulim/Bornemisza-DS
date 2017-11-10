b1225309c76685bb21d776736f1038f71dea5a20
Ulrich Mayring <ulim@mayring.de>
2017-11-10 21:14:35 +0100

    shellcheck fixes

--- salt/app/srv/salt/files/payara/domain-config.sh
+++ salt/app/srv/salt/files/payara/domain-config.sh
@@ -5,7 +5,7 @@
 # $3 the domain serviced by this Payara instance
 # $4 the fully qualified domain name of this Payara instance
 
-{%- set ASADMIN_CMD='$1 --interactive=false --user admin --passwordfile=$2' %}
+{%- set ASADMIN_CMD='$1 --interactive=false --user admin --passwordfile="$2"' %}
 
 # create alias for couchdb admin password
 {{ ASADMIN_CMD }} delete-password-alias couchdb-admin-password
@@ -13,19 +13,15 @@
 
 # create custom JNDI resource for CouchDB _users database
 {{ ASADMIN_CMD }} delete-custom-resource couchdb/Users
-#{{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/_users --restype de.bornemisza.couchdb.da.ConnectionPool --factoryclass de.bornemisza.couchdb.da.ConnectionPoolFactory couchdb/Users
 {{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/_users --restype de.bornemisza.loadbalancer.LoadBalancerConfig --factoryclass de.bornemisza.loadbalancer.LoadBalancerConfigFactory lbconfig/CouchUsersPool
 
 {{ ASADMIN_CMD }} delete-custom-resource couchdb/admin
-#{{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/_users:username=admin:password='\$\{ALIAS\=couchdb-admin-password\}' --restype de.bornemisza.couchdb.da.ConnectionPool --factoryclass de.bornemisza.couchdb.da.ConnectionPoolFactory couchdb/admin
 {{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/_users:username=admin:password='\$\{ALIAS\=couchdb-admin-password\}' --restype de.bornemisza.loadbalancer.LoadBalancerConfig --factoryclass de.bornemisza.loadbalancer.LoadBalancerConfigFactory lbconfig/CouchAdminPool
 
 {{ ASADMIN_CMD }} delete-custom-resource http/Base
-#{{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/ --restype de.bornemisza.rest.da.HttpPool --factoryclass de.bornemisza.rest.da.HttpPoolFactory http/Base
 {{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/ --restype de.bornemisza.loadbalancer.LoadBalancerConfig --factoryclass de.bornemisza.loadbalancer.LoadBalancerConfigFactory lbconfig/HttpBase
 
 {{ ASADMIN_CMD }} delete-custom-resource http/Sessions
-#{{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/_session --restype de.bornemisza.rest.da.HttpPool --factoryclass de.bornemisza.rest.da.HttpPoolFactory http/Sessions
 {{ ASADMIN_CMD }} create-custom-resource --property service=_db._tcp.$3.:db=/_session --restype de.bornemisza.loadbalancer.LoadBalancerConfig --factoryclass de.bornemisza.loadbalancer.LoadBalancerConfigFactory lbconfig/HttpSessions
 
 # create SMTP resource
@@ -34,4 +30,4 @@
 
 # create FQDN system property
 {{ ASADMIN_CMD }} delete-system-property FQDN
-{{ ASADMIN_CMD }} create-system-properties FQDN=$4
+{{ ASADMIN_CMD }} create-system-properties FQDN="$4"
