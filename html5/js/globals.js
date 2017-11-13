6e2c9cfc87d948ce22158ddd017c426906727331
Ulrich Mayring <ulim@mayring.de>
2017-11-13 21:55:23 +0100

    restrict input to safe characters

--- html5/js/globals.js
+++ html5/js/globals.js
@@ -33,3 +33,22 @@ function shakeElement(elem) {
         elem.classList.remove("shake");
     }, 1000);
 }
+
+function inputLimiter(e, allow) {
+    var AllowableCharacters = '';
+
+    if (allow == 'Letters') { AllowableCharacters = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; }
+    if (allow == 'Numbers') { AllowableCharacters = '1234567890'; }
+    if (allow == 'NameCharacters') { AllowableCharacters = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890@-._/"'; }
+
+    var k = document.all ? parseInt(e.keyCode) : parseInt(e.which);
+    if (k != 13 && k != 8 && k != 0) {
+        if ((e.ctrlKey == false) && (e.altKey == false)) {
+            return (AllowableCharacters.indexOf(String.fromCharCode(k)) != -1);
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
