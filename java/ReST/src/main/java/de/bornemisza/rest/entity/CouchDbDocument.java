4ef34611ff0ea8b6f0797890628d1aa61397b350
Ulrich Mayring <ulim@mayring.de>
2017-11-19 00:22:29 +0100

    moved here from CouchDB project

---
+++ java/ReST/src/main/java/de/bornemisza/rest/entity/CouchDbDocument.java
@@ -0,0 +1,69 @@
+package de.bornemisza.rest.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(Include.NON_NULL)
+public class CouchDbDocument implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String revision;
+    private List<String> conflicts;
+
+    @JsonProperty("_id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("_id")
+    public void setId(String s) {
+        if (s == null || s.equals("")) throw new IllegalArgumentException("id must have a value");
+        if (id != null && id.equals(s)) {
+            return;
+        }
+        if (id != null) {
+            throw new IllegalStateException("cannot set id, id already set");
+        }
+        id = s;
+    }
+
+    @JsonProperty("_rev")
+    public String getRevision() {
+        return revision;
+    }
+
+    @JsonProperty("_rev")
+    public void setRevision(String s) {
+        // no empty strings thanks
+        if (s != null && s.length() == 0) {
+            return;
+        }
+        this.revision = s;
+    }
+
+    @JsonIgnore
+    public boolean isNew() {
+        return revision == null;
+    }
+
+    @JsonProperty("_conflicts")
+    void setConflicts(List<String> conflicts) {
+        this.conflicts = conflicts;
+    }
+
+    @JsonIgnore
+    public List<String> getConflicts() {
+        return conflicts;
+    }
+
+    public boolean hasConflict() {
+        return conflicts != null && !conflicts.isEmpty();
+    }
+
+}
---
+++ java/ReST/src/main/java/de/bornemisza/rest/entity/User.java
@@ -0,0 +1,99 @@
+package de.bornemisza.rest.entity;
+
+import java.util.List;
+
+import javax.mail.internet.InternetAddress;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User extends CouchDbDocument {
+
+    public static String USERNAME_PREFIX = "org.couchdb.user:"; // CouchDB wants this
+
+    public User() {
+        // JAXB needs this
+        super();
+    }
+
+    @JsonProperty(value = "type")
+    private String type = "user";
+
+    @JsonProperty(value = "name")
+    private String name;
+
+    @JsonProperty(value = "password")
+    private char[] password;
+
+    @JsonProperty(value = "email")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private InternetAddress email;
+
+    @JsonProperty(value = "roles")
+    private List<String> roles;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name not null
+     */
+    public void setName(String name) {
+        this.name = name;
+        setId(USERNAME_PREFIX + name);
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password not null
+     */
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    public InternetAddress getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email not null
+     */
+    public void setEmail(InternetAddress email) {
+        this.email = email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    /**
+     * @param roles not null
+     */
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "CouchDbDocument{" + "id=" + getId() + ", rev=" + getRevision() + ", conflicts=" + getConflicts() + "}" +
+               "User{" + "type=" + type + ", name=" + name + ", password=******" + ", email=" + email + ", roles=" + roles + '}';
+    }
+
+}
