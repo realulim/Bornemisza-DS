32b363d8d1e04a26bc11746227368eb99e6807f5
Ulrich Mayring <ulim@mayring.de>
2017-11-19 23:25:27 +0100

    fix

--- salt/common/bootstrap-common.sh
+++ salt/common/bootstrap-common.sh
@@ -37,8 +37,8 @@ if ! grep -q service: $PillarLocal/basics.sls ; then
 fi
 
 # cluster sizes need to be rewritten on every run
-APPCLUSTERSIZE=$(host -t srv _app._tcp.$domain. $cfns|wc -l)
-DBCLUSTERSIZE=$(host -t srv _db._tcp.$domain. $cfns|wc -l)
+APPCLUSTERSIZE=$(host -t srv _app._tcp.$domain. $cfns|grep SRV|wc -l)
+DBCLUSTERSIZE=$(host -t srv _db._tcp.$domain. $cfns|grep SRV|wc -l)
 printf "appclustersize: %s\ndbclustersize: %s\n" "$APPCLUSTERSIZE" "$DBCLUSTERSIZE" | tee $PillarLocal/cluster.sls
 
 # ask for Cloudflare API key
