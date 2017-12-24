591eb3d7ec92874f2f5360397fc78ac107c5fac1
Ulrich Mayring <ulim@mayring.de>
2017-12-24 23:38:22 +0100

    implement loadColors

--- java/Sessions/src/main/java/de/bornemisza/sessions/endpoint/Uuids.java
+++ java/Sessions/src/main/java/de/bornemisza/sessions/endpoint/Uuids.java
@@ -13,6 +13,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import de.bornemisza.rest.HttpHeaders;
+import de.bornemisza.rest.entity.result.KeyValueViewResult;
 import de.bornemisza.rest.entity.result.UuidsResult;
 import de.bornemisza.rest.exception.UnauthorizedException;
 import de.bornemisza.rest.security.Auth;
@@ -42,6 +43,24 @@ public class Uuids {
         try {
             Auth auth = new Auth(new DoubleSubmitToken(cookie, ctoken));
             UuidsResult result = facade.getUuids(auth, count);
+            return result.toResponse();
+        }
+        catch (UnauthorizedException ex) {
+            return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
+        }
+        catch (RuntimeException ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
+    @GET
+    @Path("colors/stats")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response loadColors(@HeaderParam(HttpHeaders.COOKIE) String cookie,
+                               @HeaderParam(HttpHeaders.CTOKEN) String ctoken) {
+        try {
+            Auth auth = new Auth(new DoubleSubmitToken(cookie, ctoken));
+            KeyValueViewResult result = facade.loadColors(auth);
             return result.toResponse();
         }
         catch (UnauthorizedException ex) {
