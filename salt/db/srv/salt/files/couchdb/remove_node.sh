163dda17212879f5e25f5a194fa2bd6ace4dabb7
Ulrich Mayring <ulim@mayring.de>
2018-01-10 00:39:58 +0100

    fix

--- salt/db/srv/salt/files/couchdb/remove_node.sh
+++ salt/db/srv/salt/files/couchdb/remove_node.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-
+set -x
 # $1 - the ip address of the node to be removed from the cluster
 
 # Example: ./remove_node.sh 10.4.7.37
 
 AUTH=$(cat /srv/pillar/netrc)
+INORIG=/tmp/shardsorig.json
 IN=/tmp/shardsold.json
 OUT=/tmp/shardsnew.json
 
 for DB in $(curl -s http://"$1":5984/_all_dbs|jq -re '.[]'|grep -v _)
 do
 
-# retrieve shards document
-curl -s -u "$AUTH" http://localhost:5986/_dbs/"$DB"|jq 'del(.by_node|."couchdb@$1")'|jq 'del(.by_range|.[]|.[]|select(.|contains("@$1")))' > $IN
+BY_NODE="del(.by_node|.\"couchdb@$1\")"
+BY_RANGE="del(.by_range|.[]|.[]|select(.|contains(\"@$1\")))"
 
-SHARDS=$(jq -e '.by_node' "$IN" | tail -n +3 | sed '/],/,$d')
+# retrieve shards document
+curl -s -u "$AUTH" http://localhost:5986/_dbs/"$DB" > $INORIG
+curl -s -u "$AUTH" http://localhost:5986/_dbs/"$DB"|jq "$BY_NODE"|jq "$BY_RANGE" > $IN
+
 CHANGELOG=$(jq -e '.changelog' $IN | head -n -1 | tail -n +2)
+SHARDS=$(jq -e '.by_node' "$INORIG" | tail -n +3 | sed '/],/,$d' | tr -d ,)
 
 echo { > $OUT
 
@@ -24,16 +29,19 @@ echo "\"_rev\":" $(jq -e '._rev' $IN), >> $OUT
 echo "\"shard_suffix\":" $(jq -e '.shard_suffix' $IN), >> $OUT
 
 echo "\"changelog\": [" >> $OUT
-echo $CHANGELOG , >> $OUT
+echo $CHANGELOG >> $OUT
 
      for SHARD in $SHARDS
      do
-          echo ", ["
-          echo "    \"remove\","
-          echo "    $SHARD"
-          echo "    \"couchdb@$1\""
-          echo "  ]"
+          echo ",[ \"remove\", $SHARD, \"couchdb@$1\" ]" >> $OUT
      done
+echo "]", >> $OUT
+
+echo "\"by_node\": $(jq -e '.by_node' $IN)," >> $OUT
+
+echo "\"by_range\": $(jq -e '.by_range' $IN)" >> $OUT
+
+echo "}" >> $OUT
 
 done
 
