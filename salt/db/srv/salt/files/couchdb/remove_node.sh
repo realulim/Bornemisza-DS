d2eb825ee7def3420b9174f90d6d20b1694d57c4
Ulrich Mayring <ulim@mayring.de>
2018-01-09 23:30:27 +0100

    clean up shards document

--- salt/db/srv/salt/files/couchdb/remove_node.sh
+++ salt/db/srv/salt/files/couchdb/remove_node.sh
@@ -8,6 +8,35 @@ AUTH=$(cat /srv/pillar/netrc)
 IN=/tmp/shardsold.json
 OUT=/tmp/shardsnew.json
 
+for DB in $(curl -s http://"$1":5984/_all_dbs|jq -re '.[]'|grep -v _)
+do
+
+# retrieve shards document
+curl -s -u "$AUTH" http://localhost:5986/_dbs/"$DB"|jq 'del(.by_node|."couchdb@$1")'|jq 'del(.by_range|.[]|.[]|select(.|contains("@$1")))' > $IN
+
+SHARDS=$(jq -e '.by_node' "$IN" | tail -n +3 | sed '/],/,$d')
+CHANGELOG=$(jq -e '.changelog' $IN | head -n -1 | tail -n +2)
+
+echo { > $OUT
+
+echo "\"_id\":" $(jq -e '._id' $IN), >> $OUT
+echo "\"_rev\":" $(jq -e '._rev' $IN), >> $OUT
+echo "\"shard_suffix\":" $(jq -e '.shard_suffix' $IN), >> $OUT
+
+echo "\"changelog\": [" >> $OUT
+echo $CHANGELOG , >> $OUT
+
+     for SHARD in $SHARDS
+     do
+          echo ", ["
+          echo "    \"remove\","
+          echo "    $SHARD"
+          echo "    \"couchdb@$1\""
+          echo "  ]"
+     done
+
+done
+
 # retrieve revision of node
 REV=$(/usr/bin/curl -s -u "$AUTH" http://localhost:5986/_nodes/couchdb@$1 | jq -re '._rev')
 
